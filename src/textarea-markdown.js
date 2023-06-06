72e8ac872cca69be49623b8f25d70e5c4385e941
YukiWatanabe824 <gyggg950@gmail.com>
2023-06-06 23:37:36 +0900

    fix:file-type

--- src/textarea-markdown.js
+++ src/textarea-markdown.js
@@ -102,7 +102,7 @@ export default class TextareaMarkdown {
     reader.readAsArrayBuffer(file);
     reader.onload = async () => {
       const bytes = new Uint8Array(reader.result);
-      const fileType = await FileType.fromStream(bytes)["ext"];
+      const fileType = await FileType.fromStream(bytes);
       const fileSize = filesize(file.size, { base: 10, standard: "jedec" });
       const text =
         "![" +
@@ -138,12 +138,12 @@ export default class TextareaMarkdown {
         .then((json) => {
           const responseKey = this.options["responseKey"];
           const url = json[responseKey];
-          if (this.options["imageableExtensions"].includes(fileType)) {
+          if (this.options["imageableExtensions"].includes(fileType.ext)) {
             this.textarea.value = this.textarea.value.replace(
               text,
               `![${file.name}](${url})\n`
             );
-          } else if (this.options["videoExtensions"].includes(fileType)) {
+          } else if (this.options["videoExtensions"].includes(fileType.ext)) {
             this.textarea.value = this.textarea.value.replace(
               text,
               `<video controls src="${url}"></video>\n`
